150f6581fe5037b4d2e17d7a88457b0ecdd09957
JP <jp@jp.nl>
2025-02-24 11:05:14 +0100

    Projecten gegenereerd, eerste pagina opgezet in DemoProject

---
+++ BlazorDemos/DemoProject/Components/Pages/Cursussen.razor.cs
@@ -0,0 +1,31 @@
+﻿using DemoProject.Entities;
+
+namespace DemoProject.Components.Pages
+{
+    public partial class Cursussen
+    {
+        public string Naam { get; set; } = "JP";
+
+        public List<Course>? Courses { get; set; } = new()
+        {
+            new()
+            {
+                Title = "Awesome Blazorrrr",
+                NrOfDays = 5,
+                FunPhoto = "https://www.delta-n.nl/wp-content/uploads/2019/10/BrandBlazor_300.png"
+            },
+            new()
+            {
+                Title = "Awesome C#",
+                NrOfDays = 5,
+                FunPhoto = "https://miro.medium.com/v2/resize:fit:375/1*NhpIIUL7AFgKKn30gKoDUw.png"
+            },
+            new()
+            {
+                Title = "Matige React",
+                NrOfDays = 2,
+                FunPhoto = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png"
+            }
+        };
+    }
+}
---
+++ BlazorDemos/DemoProject/Entities/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoProject.Entities;
+
+public class Course
+{
+    public required string Title { get; set; }
+
+    public required byte NrOfDays { get; set; }
+
+    public required string FunPhoto { get; set; }
+}
