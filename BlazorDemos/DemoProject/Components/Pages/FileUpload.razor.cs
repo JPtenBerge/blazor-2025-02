a70faa6be359c14899a2c7b1d89b7b7a74cf45e3
JP <jp@jp.nl>
2025-02-25 10:04:58 +0100

    File upload laten zien

---
+++ BlazorDemos/DemoProject/Components/Pages/FileUpload.razor.cs
@@ -0,0 +1,19 @@
+using DemoProject.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace DemoProject.Components.Pages;
+
+public partial class FileUpload : ComponentBase
+{
+    [SupplyParameterFromForm]
+    public Park NewPark { get; set; } = new();
+
+
+    public async Task Process()
+    {
+        using var stream = NewPark.AlgemeneVoorwaarden.OpenReadStream();
+        using var reader = new StreamReader(stream);
+        var algVoor = await reader.ReadToEndAsync();
+        Console.WriteLine($"algemene voorwaarden: {algVoor}");
+    }
+}
---
+++ BlazorDemos/DemoProject/Entities/Park.cs
@@ -0,0 +1,7 @@
+namespace DemoProject.Entities;
+
+public class Park
+{
+    public string Name { get; set; }
+    public IFormFile AlgemeneVoorwaarden { get; set; }
+}
